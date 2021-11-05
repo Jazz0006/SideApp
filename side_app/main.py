caaf03a3d8ee05e570b7491becf1f5aa0eb135f2
JazzZeng <jazzzeng@LAPTOP-ASUS.localdomain>
2021-11-05 12:47:15 +1100

    upto to unittest

--- side_app/main.py
+++ side_app/main.py
@@ -1,48 +1,31 @@
-from operator import countOf
-import os
+from flask_marshmallow import Marshmallow
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow.exceptions import ValidationError
+
+db = SQLAlchemy()
+ma = Marshmallow()
 
 def create_app():
 
     app = Flask(__name__)
     app.config.from_object("config.app_config")
 
-    db = SQLAlchemy(app)
+    #db = SQLAlchemy(app)
+    db.init_app(app)
+    ma.init_app(app)
 
-    class Course(db.Model):
-        __tablename__ = "courses"
-        course_id = db.Column(db.Integer, primary_key = True)
-        course_name = db.Column(db.String(80), unique = True, nullable = False)
+    from commands import db_commands
+    app.register_blueprint(db_commands)
 
-        def __init__(self, course_name):
-            self.course_name = course_name
-
-        @property
-        def serialize(self):
-            return {
-                "course_id": self.course_id,
-                "course_name": self.course_name
-            }
-
-    db.create_all()
-
-    @app.route('/')
-    def homepage():
-        return "Hello, world! Check this out!"
-
-    @app.route("/courses/", methods = ["GET"])
-    def get_courses():
-        courses = Course.query.all()
-        return jsonify([course.serialize for course in courses])
-
-    @app.route('/students/')
-    def get_students():
-        return "This is all students..."
-
-    @app.route('/students/<int:student_id>')
-    def get_specific_students(student_id):
-        return f"This is a apge displaying information about student {student_id}."
+    #db.create_all()
+    from controllers import registerable_controllers
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+    
+    @app.errorhandler(ValidationError)
+    def handle_bad_request(error):
+        return (jsonify(error.messages), 400)
 
     return app
     #app.run(debug=True)
